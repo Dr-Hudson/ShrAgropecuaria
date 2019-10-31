0944c0274370335f6c55f967a58e65e9690dc191
Hudson <Hudson_Trombeta@outlook.com>
2019-10-30 22:08:21 -0300

    V0.00

---
+++ ShrAgropecuaria/Program.cs
@@ -0,0 +1,23 @@
+﻿using ShrAgropecuaria.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjAvaliacao2Bim
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new view_Menu());
+        }
+    }
+}
