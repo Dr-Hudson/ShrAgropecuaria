5dc721e5c3ba2cf99181373a02a46ee24ca4da09
Hudson <Hudson_Trombeta@outlook.com>
2019-11-05 13:22:45 -0300

    Classe Cliente Criada

--- ShrAgropecuaria/Classes/Cliente.cs
+++ ShrAgropecuaria/Classes/Cliente.cs
@@ -8,5 +8,36 @@ namespace ShrAgropecuaria.Classes
 {
     public class Cliente
     {
+        int? cli_cod;
+        DateTime cli_dataliberacao;
+        decimal cli_limite;
+        decimal cli_saldofiado;
+        string cli_bairro;
+        string cli_cep;
+        string cli_complemento;
+        decimal cli_numero;
+        string cli_nome;
+        string cli_cpf;
+        string cli_rg;
+        string cli_endereco;
+        string cli_telefone;
+        Cidade cidade;
+
+        public int? Cli_cod { get => cli_cod; set => cli_cod = value; }
+        public DateTime Cli_dataliberacao { get => cli_dataliberacao; set => cli_dataliberacao = value; }
+        public decimal Cli_limite { get => cli_limite; set => cli_limite = value; }
+        public decimal Cli_saldofiado { get => cli_saldofiado; set => cli_saldofiado = value; }
+        public string Cli_bairro { get => cli_bairro; set => cli_bairro = value; }
+        public string Cli_cep { get => cli_cep; set => cli_cep = value; }
+        public string Cli_complemento { get => cli_complemento; set => cli_complemento = value; }
+        public decimal Cli_numero { get => cli_numero; set => cli_numero = value; }
+        public string Cli_nome { get => cli_nome; set => cli_nome = value; }
+        public string Cli_cpf { get => cli_cpf; set => cli_cpf = value; }
+        public string Cli_rg { get => cli_rg; set => cli_rg = value; }
+        public string Cli_endereco { get => cli_endereco; set => cli_endereco = value; }
+        public string Cli_telefone { get => cli_telefone; set => cli_telefone = value; }
+        public Cidade Cidade { get => cidade; set => cidade = value; }
+
+        public int? Cidadeid { get { return Cidade?.Cid_cod; } }
     }
 }
--- ShrAgropecuaria/Classes/Usuario.cs
+++ ShrAgropecuaria/Classes/Usuario.cs
@@ -16,7 +16,7 @@ namespace ShrAgropecuaria.Classes
 
         public int? User_cod { get => user_cod; set => user_cod = value; }
         public int User_nivel { get => user_nivel; set => user_nivel = value; }
-        public char user_status { get => user_status; set => user_status = value; }
+        public char User_status { get => user_status; set => user_status = value; }
         public string User_login { get => user_login; set => user_login = value; }
         public string User_senha { get => user_senha; set => user_senha = value; }
     }
