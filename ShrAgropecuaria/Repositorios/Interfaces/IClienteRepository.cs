51e7c2ac8613a430733c56cf533def21d65875ee
Hudson <Hudson_Trombeta@outlook.com>
2019-11-05 13:57:10 -0300

    Feito o Repositorio do Cliente

---
+++ ShrAgropecuaria/Repositorios/Interfaces/IClienteRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShrAgropecuaria.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShrAgropecuaria.Repositorios.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Cliente Get(int? id);
+        IEnumerable<Cliente> GetAll(string nome);
+        IEnumerable<Cliente> GetByNome(string Nome);
+        IEnumerable<Cliente> GetById(int id);
+        Cliente PegaId(string nome);
+        void Gravar(Cliente cli);
+        void Excluir(Cliente cli);
+    }
+}
---
+++ ShrAgropecuaria/Repositorios/MySqlRepository/MySqlClienteRepository.cs
@@ -0,0 +1,95 @@
+﻿using Dapper;
+using ShrAgropecuaria.Classes;
+using ShrAgropecuaria.Repositorios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShrAgropecuaria.Repositorios.MySqlRepository
+{
+    class MySqlClienteRepository : MySqlRepository, IClienteRepository
+    {
+        public MySqlClienteRepository(IDbConnection connection) : base(connection)
+        {
+
+        }
+
+        public void Excluir(Cliente cli)
+        {
+            Connection.Execute("delete from cliente where cli_cod = @cli_cod", cli);
+        }
+
+        public Cliente Get(int? id)
+        {
+            if (id != null)
+            {
+                return Connection.Query<Cliente>("select * from cliente where cli_cod = @id", new { id }).FirstOrDefault();
+            }
+            return null;
+        }
+
+        public void Gravar(Cliente cli)
+        {
+            if (cli.Cli_cod == null)
+            {
+                Connection.Execute("INSERT INTO cliente " +
+                    "(cli_cod, cli_dataliberacao, cli_limite, cli_saldofiado, cli_bairro, cli_cep, cli_complemento, cli_numero, cli_nome, cli_cpf, cli_rg, cli_endereco, cli_telefone, cid_cod) " + 
+                    "VALUES (@cli_cod, @cli_dataliberacao, @cli_limite, @cli_saldofiado, @cli_bairro, @cli_cep, @cli_complemento, @cli_numero, @cli_nome, @cli_cpf, @cli_rg, @cli_endereco, " +
+                    " @cli_telefone, @cid_cod)", cli);
+            }
+            else
+            {
+                Connection.Execute("UPDATE cliente SET cli_dataliberacao = @cli_dataliberacao, cli_limite = @cli_limite, cli_saldofiado = @cli_saldofiado, cli_bairro = @cli_bairro," +
+                    "cli_cep = @cli_cep, cli_complemento = @cli_complemento, cli_numero = @cli_numero, cli_nome = @cli_nome, cli_cpf = @cli_cpf, cli_rg = @cli_rg, cli_endereco = @cli_endereco, " +
+                    "cli_telefone = @cli_telefone, cid_cod = @cid_cod WHERE cli_cod = @cli_cod;", cli);
+            }
+        }
+
+        public Cliente PegaId(string nome)
+        {
+            string sql = @"select cli.*, cid.* from cliente cli
+                            inner join cidade cid on cid.cid_cod = cli.cid_cod
+                            where cli.cli_nome like @nome";
+            return Connection.Query<Cliente>(sql, new { nome = "'%" + nome + "%'" }).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> GetAll(string nome)
+        {
+            string sql = @"select cli.*, cid.* from cliente cli
+                            inner join cidade cid on cid.cid_cod = cli.cid_cod
+                            where forn.cli_nome like @nome";
+            return Connection.Query<Cliente, Cidade, Cliente>(sql, (cliente, cidade) =>
+            {
+                cliente.Cidade = cidade;
+                return cliente;
+            }, new { nome = "'%" + nome + "%'" });
+        }
+
+        public IEnumerable<Cliente> GetById(int id)
+        {
+            string sql = @"select cli.*, cid.* from cliente cli
+                            inner join cidade cid on cid.cid_cod = cli.cid_cod
+                            where cli.cli_cod = @id";
+            return Connection.Query<Cliente, Cidade, Cliente>(sql, (cliente, cidade) =>
+            {
+                cliente.Cidade = cidade;
+                return cliente;
+            }, new { id });
+        }
+
+        public IEnumerable<Cliente> GetByNome(string Nome)
+        {
+            string sql = @"select cli.*, cid.* from cliente cli
+                            inner join cidade cid on cid.cid_cod = cli.cid_cod
+                            where forn.cli_nome like @nome";
+            return Connection.Query<Cliente, Cidade, Cliente>(sql, (cliente, cidade) =>
+            {
+                cliente.Cidade = cidade;
+                return cliente;
+            }, new { nome = "'%" + Nome + "%'" });
+        }
+    }
+}
