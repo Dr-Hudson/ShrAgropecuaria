1414f52a024c18a6290d91840ed71f2074c55dab
GustavoDillio <gustavodillio@gmail.com>
2019-11-05 12:42:05 -0200

    Reository criado mais incompleto do usuario

---
+++ ShrAgropecuaria/Repositorios/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShrAgropecuaria.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShrAgropecuaria.Repositorios.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Usuario Get(int? id);
+        IEnumerable<Usuario> GetAll(string nome);
+        IEnumerable<Usuario> GetByNome(string Nome);
+        Usuario PegaId(string nome);
+        void Gravar(Usuario u);
+        void Excluir(Usuario u);
+    }
+}
+
---
+++ ShrAgropecuaria/Repositorios/MySqlRepository/MysSqlUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using ShrAgropecuaria.Classes;
+using ShrAgropecuaria.Repositorios.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShrAgropecuaria.Repositorios.MySqlRepository
+{
+    class MySqlUsuarioRepository : MySqlRepository, IUsuarioRepository
+    {
+
+    }
+}
