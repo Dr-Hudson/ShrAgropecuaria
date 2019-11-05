9555e6efb093a19411c03a699d7882e7b5b65e68
GustavoDillio <gustavodillio@gmail.com>
2019-11-05 12:28:37 -0200

    Class de Usuario.

--- ShrAgropecuaria/Classes/Usuario.cs
+++ ShrAgropecuaria/Classes/Usuario.cs
@@ -8,5 +8,16 @@ namespace ShrAgropecuaria.Classes
 {
     class Usuario
     {
+        int? user_cod;
+        int user_nivel;
+        char user_status;
+        string user_login;
+        string user_senha;
+
+        public int? User_cod { get => user_cod; set => user_cod = value; }
+        public int User_nivel { get => user_nivel; set => user_nivel = value; }
+        public char user_status { get => user_status; set => user_status = value; }
+        public string User_login { get => user_login; set => user_login = value; }
+        public string User_senha { get => user_senha; set => user_senha = value; }
     }
 }
