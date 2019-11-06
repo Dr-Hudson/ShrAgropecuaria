e1383d908b6559dde57c2f32d805dcfcd793a78c
Gustavo <Aluno@LF104M09-71600>
2019-11-06 20:00:25 -0200

    Mds

--- ShrAgropecuaria/Views/view_ProdutoNutrição.Designer.cs
+++ ShrAgropecuaria/Views/view_ProdutoNutrição.Designer.cs
@@ -28,27 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.txtValor = new System.Windows.Forms.MaskedTextBox();
+            this.dtpPrevisao = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtObs = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btPesquisar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btGravar = new System.Windows.Forms.Button();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.lbR = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtID
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(42, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtID.Location = new System.Drawing.Point(12, 31);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(42, 20);
+            this.txtID.TabIndex = 0;
             // 
-            // textBox2
+            // txtNome
             // 
-            this.textBox2.Location = new System.Drawing.Point(100, 31);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(280, 20);
-            this.textBox2.TabIndex = 1;
+            this.txtNome.Location = new System.Drawing.Point(100, 31);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(280, 20);
+            this.txtNome.TabIndex = 1;
             // 
             // label1
             // 
@@ -77,27 +88,136 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Valor unitario";
             // 
-            // maskedTextBox1
+            // txtValor
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(15, 98);
-            this.maskedTextBox1.Mask = "000000.00";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox1.TabIndex = 6;
-            this.maskedTextBox1.ValidatingType = typeof(int);
-            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.MaskedTextBox1_MaskInputRejected);
+            this.txtValor.Location = new System.Drawing.Point(15, 88);
+            this.txtValor.Mask = "000000.00";
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 6;
+            this.txtValor.ValidatingType = typeof(int);
+            // 
+            // dtpPrevisao
+            // 
+            this.dtpPrevisao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpPrevisao.Location = new System.Drawing.Point(144, 88);
+            this.dtpPrevisao.Name = "dtpPrevisao";
+            this.dtpPrevisao.Size = new System.Drawing.Size(91, 20);
+            this.dtpPrevisao.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(141, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Previsão de entrega";
+            // 
+            // txtObs
+            // 
+            this.txtObs.Location = new System.Drawing.Point(15, 141);
+            this.txtObs.Name = "txtObs";
+            this.txtObs.Size = new System.Drawing.Size(368, 60);
+            this.txtObs.TabIndex = 9;
+            this.txtObs.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Observação";
+            // 
+            // cbTipo
+            // 
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Location = new System.Drawing.Point(261, 86);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(121, 21);
+            this.cbTipo.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(258, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Tipo produto";
+            // 
+            // btPesquisar
+            // 
+            this.btPesquisar.Location = new System.Drawing.Point(117, 265);
+            this.btPesquisar.Name = "btPesquisar";
+            this.btPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btPesquisar.TabIndex = 17;
+            this.btPesquisar.Text = "Pesquisar";
+            this.btPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.Location = new System.Drawing.Point(276, 265);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btExcluir.TabIndex = 16;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btGravar
+            // 
+            this.btGravar.Location = new System.Drawing.Point(195, 265);
+            this.btGravar.Name = "btGravar";
+            this.btGravar.Size = new System.Drawing.Size(75, 23);
+            this.btGravar.TabIndex = 15;
+            this.btGravar.Text = "Gravar";
+            this.btGravar.UseVisualStyleBackColor = true;
+            this.btGravar.Click += new System.EventHandler(this.BtGravar_Click);
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(36, 265);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpar.TabIndex = 14;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.BtLimpar_Click);
+            // 
+            // lbR
+            // 
+            this.lbR.AutoSize = true;
+            this.lbR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbR.ForeColor = System.Drawing.Color.Red;
+            this.lbR.Location = new System.Drawing.Point(12, 220);
+            this.lbR.Name = "lbR";
+            this.lbR.Size = new System.Drawing.Size(0, 17);
+            this.lbR.TabIndex = 18;
             // 
             // view_ProdutoNutrição
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.maskedTextBox1);
+            this.ClientSize = new System.Drawing.Size(401, 300);
+            this.Controls.Add(this.lbR);
+            this.Controls.Add(this.btPesquisar);
+            this.Controls.Add(this.btExcluir);
+            this.Controls.Add(this.btGravar);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbTipo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtObs);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtpPrevisao);
+            this.Controls.Add(this.txtValor);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtID);
             this.Name = "view_ProdutoNutrição";
             this.Text = "view_ProdutoNutrição";
             this.ResumeLayout(false);
@@ -107,11 +227,22 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox txtValor;
+        private System.Windows.Forms.DateTimePicker dtpPrevisao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox txtObs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbTipo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btPesquisar;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Button btGravar;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Label lbR;
     }
 }
