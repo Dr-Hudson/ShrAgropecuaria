bd6eb8485ff26e8012a9e3bb853dcaf3b1f0a951
Hudson <Hudson_Trombeta@outlook.com>
2019-11-20 15:31:28 -0300

    Tem que refazer o Menu

--- ShrAgropecuaria/Views/view_Menu.cs
+++ ShrAgropecuaria/Views/view_Menu.cs
@@ -1,5 +1,4 @@
-﻿using ShrAgropecuaria.Classes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,61 +16,5 @@ namespace ShrAgropecuaria.Views
         {
             InitializeComponent();
         }
-
-        private void FornecedoresToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_Fornecedor>();
-            f.Show();
-        }
-
-        private void ClienteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<View_Cliente>();
-            f.Show();
-        }
-
-        private void ProdutosPETToolStripMenuItem_Click_1(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_ProdutoPET>();
-            f.Show();
-        }
-
-        private void UsuarioToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_Usuario>();
-            f.Show();
-        }
-
-        private void ProdutoNutriçãoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_ProdutoNutrição>();
-            f.Show();
-        }
-
-        private void ParametrizacaoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_Parametrização>();
-            f.Show();
-
-        }
-
-        private void FiadoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_Fiado>();
-            f.Show();
-        }
-
-        private void lançarDespesaToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_LancarDespesa>();
-            f.Show();
-        }
-
-        private void entregaPedidoNutriçãoToolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            var f = Dependencia.Container.GetInstance<view_ControlarEntregaPedidoNutrição>();
-            f.Show();
-        }
-
     }
 }
