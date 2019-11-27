4faf4b68ab1e8944fe84263ce52a1edbe2095a72
Renanzera <Renan@DESKTOP-PK3RH0F>
2019-11-27 15:08:50 -0200

    aaaaaaaaa

--- ShrAgropecuaria/Repositorios/Interfaces/IVendaPETRepository.cs
+++ ShrAgropecuaria/Repositorios/Interfaces/IVendaPETRepository.cs
@@ -13,6 +13,9 @@ namespace ShrAgropecuaria.Repositorios.Interfaces
         void GravarProds(ProdutoVenda pv);
         void atualizarproduto(int cod, int n);
 
+        void Exclui(ProdutoVenda p, VendaPET v);
+        void Update(ProdutoVenda p);
+
         IEnumerable<VendaPET> getALL(string nome);
 
         IEnumerable<ProdutoVenda> GetPVenda(int? cod);
